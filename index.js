5c74f2fbbb0c65c67fde609b4c5b73033d21befd
kaue <kauesacra@hotmail.com>
2024-05-17 17:31:51 -0300

    hotfix - complement value

--- index.js
+++ index.js
@@ -17,19 +17,23 @@ document.addEventListener("DOMContentLoaded", function (event) {
 });
 
 function showTab(n, tab, buttonPrev, buttonNext, step) {
-    var x = document.getElementsByClassName(tab);
-    x[n].style.display = "block";
-    if (n == 0) {
-        document.getElementById(buttonPrev).style.display = "none";
-    } else {
-        document.getElementById(buttonPrev).style.display = "inline";
+    try {
+        var x = document.getElementsByClassName(tab);
+        x[n].style.display = "block";
+        if (n == 0) {
+            document.getElementById(buttonPrev).style.display = "none";
+        } else {
+            document.getElementById(buttonPrev).style.display = "inline";
+        }
+        if (n == (x.length - 1)) {
+            document.getElementById(buttonNext).innerHTML = "Enviar";
+        } else {
+            document.getElementById(buttonNext).innerHTML = "Próximo";
+        }
+        fixStepIndicator(n, step);
+    } catch (e) {
+        console.log(e.error)
     }
-    if (n == (x.length - 1)) {
-        document.getElementById(buttonNext).innerHTML = "Enviar";
-    } else {
-        document.getElementById(buttonNext).innerHTML = "Próximo";
-    }
-    fixStepIndicator(n, step)
 }
 
 function nextPrev(n, tab, nextPrevious, allSteps, register, textMessage, step, buttonPrev, buttonNext) {
@@ -48,14 +52,30 @@ function nextPrev(n, tab, nextPrevious, allSteps, register, textMessage, step, b
         document.getElementById(register).style.display = "none";
         document.getElementById(textMessage).style.display = "block";
 
+        // ALTERA AQUI CADA VEZ QUE CRIAR UMA FUNÇÃO NOVA , APONTE PARA A TAB DO MODAL QUE TA ABRINDO
         switch (tab) {
+            case "tab":
+                pedidoCupuacu();
+                break;
+            case "tab2":
+                pedidoSundae();
+                break;
             case "tab5":
                 pedidoCasquinha();
+                break;
+            case "tab6":
+                pedidoOutros();
+                break;
+            case "tab4":
+                pedidoMilkShake();
+                break;
+            case "tab3":
+                pedidoAcai();
+                break;
             default:
                 console.log("ainda não implementado");
 
         }
-
     }
 
     showTab(currentTab, tab, buttonPrev, buttonNext, step);
@@ -64,7 +84,7 @@ function nextPrev(n, tab, nextPrevious, allSteps, register, textMessage, step, b
 function validateForm(tab, step) {
     console.log("step " + step)
     var x, y, i, valid = true;
-    x = document.getElementsByClassName(tab);
+    x = document.getElementsByClassName(tab,);
     y = x[currentTab].getElementsByTagName("input");
     for (i = 0; i < y.length; i++) {
         if (y[i].value == "") {
@@ -86,20 +106,16 @@ function fixStepIndicator(n, step) {
 }
 
 window.onload = function () {
-
-    $(".tab4").children().children().children().children().css("font-size", 13);
-
+    $(".tab").children().children().children().children().css("font-size", 13);
     $("#exampleModa4,#exampleModa3,#exampleModa2,#exampleModal,#exampleModa5,#examplemoda6").on("hidden.bs.modal", function () {
-
         location.reload();
-
     });
 }
 
 function continuePedido() {
     location.reload();
 }
-function cancelarPedido(){
+function cancelarPedido() {
     Swal.fire({
         title: "Pedido cancelado !",
         text: "",
@@ -118,7 +134,13 @@ function finalizaPedido() {
     let valorFinal = 3;
     pedidos = JSON.parse(localStorage.getItem("pedidos"));
     pedidos.forEach((item, index) => {
-        valorFinal = item.valor + valorFinal;
+        let complements = JSON.parse(item.sabor);
+        let price = 0;
+        complements.forEach((itemComplement) => {
+            price += itemComplement.price;
+        });
+
+        valorFinal = valorFinal + item.valor + price;
     });
 
     Swal.fire({
@@ -130,21 +152,22 @@ function finalizaPedido() {
     });
 }
 
+//copiar essa funçao pra cada tipo de pedido
 function pedidoCasquinha() {
 
     let tipoCasquinha = $('input[name=tipoCasquinha]:checked').val();
-    let saborCasquinha = $('input[name=saborCasquinha]:checked').val();
+    let saborCasquinha = $('input[name=saborCasquinha]:checked').val(); //alterar aqui
 
     const casquinhas = [];
-    casquinhas["Casquinha"] = {'valor' : 4, 'descricao': 'Casquinha'};
-    casquinhas["cascao"] = {'valor' : 6, 'descricao': 'Cascão'};
-    casquinhas["Cascao_borda_chocolate"] ={'valor' : 8, 'descricao': 'Cascão c/ borda de nutella'};
+    casquinhas["Casquinha"] = { 'valor': 4, 'descricao': 'Casquinha' };
+    casquinhas["cascao"] = { 'valor': 6, 'descricao': 'Cascao' };
+    casquinhas["Cascao_borda_Nutella"] = { 'valor': 8, 'descricao': 'Cascão_borda_nutella' };
 
     let pedido = {
         "tipoPedido": tipoCasquinha,
-        "sabor": saborCasquinha,
+        "sabor": saborCasquinha, //alterar aqui
         "valor": casquinhas[tipoCasquinha].valor,
-        "descricao":  casquinhas[tipoCasquinha].descricao
+        "descricao": casquinhas[tipoCasquinha].descricao
     }
 
     let pedidos = [];
@@ -152,16 +175,198 @@ function pedidoCasquinha() {
     pedidos = localStorage.getItem("pedidos") == null ? [] : JSON.parse(localStorage.getItem("pedidos"));
     pedidos.push(pedido);
     localStorage.setItem("pedidos", JSON.stringify(pedidos));
+    publicarHtml();
+}
 
-    valorFinal = casquinhas[tipoCasquinha].valor;
+function formatNumberMonetario(valorFinal) {
+    return valorFinal.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
+}
+//function pedido outros
+function pedidoOutros() {
+
+    let tipoOutros = $('input[name=tipoOutros]:checked').val();
+    let conf_Outros = $('input[name=conf_Outros]:checked').val();
+
+    const outros = [];
+    outros["PetitGateau"] = { 'valor': 15, 'descricao': 'Petit Gateau' };
+    outros["BananaSplit"] = { 'valor': 15, 'descricao': 'Banana Split' };
+
+    let pedido = {
+        "tipoPedido": tipoOutros,
+        "sabor": conf_Outros,
+        "valor": outros[tipoOutros].valor,
+        "descricao": outros[tipoOutros].descricao
+    }
+
+    let pedidos = [];
+
+    pedidos = localStorage.getItem("pedidos") == null ? [] : JSON.parse(localStorage.getItem("pedidos"));
+    pedidos.push(pedido);
+    localStorage.setItem("pedidos", JSON.stringify(pedidos));
+    publicarHtml();
+}
+
+
+function publicarHtml() {
+    let pedidos = localStorage.getItem("pedidos") == null ? [] : JSON.parse(localStorage.getItem("pedidos"));
     let html = "";
-
     pedidos.forEach((item, index) => {
-        html += "<p  class='text-pedido-final'> Seu pedido " + (index + 1)+ " foi " + item.descricao + " com o sabor " + item.sabor + ". Valor do pedido " + formatNumberMonetario(item.valor) + "</p>";
+        let complements = JSON.parse(item.sabor);
+        let flavors = "";
+        let price = 0;
+
+        complements.forEach((itemComplement) => {
+            flavors += " , " + itemComplement.complement;
+            price += itemComplement.price;
+        });
+
+        let finalPrice = price + item.valor;
+        html += "<p  class='text-pedido-final'> Seu pedido " + (index + 1) + " foi " + item.descricao + " de sabor " + flavors + ". Valor do pedido " + formatNumberMonetario(finalPrice) + "</p>";
     });
 
-    $("#text-finaliza-pedido-casquinha").html(html);
+    $(".text-finaliza-pedido").html("");
+    $(".text-finaliza-pedido").html(html);
+}
 
+function formatNumberMonetario(valorFinal) {
+    return valorFinal.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
+}
+
+//pedido sundae
+function pedidoSundae() {
+
+    let tipoSundae = $('input[name=quantidadeSundae]:checked').val();
+    let saborSundae = returnComplementValues('.input_value_sundae'); // alterar aqui
+
+    const sundae = [];
+    sundae["300ml"] = { 'valor': 10, 'descricao': 'Sundae 300ml' };
+    sundae["400ml"] = { 'valor': 12, 'descricao': 'Sundae 400ml' };
+
+    let pedido = {
+        "tipoPedido": tipoSundae,
+        "sabor": JSON.stringify(saborSundae), //alterar aqqui
+        "valor": sundae[tipoSundae].valor,
+        "descricao": sundae[tipoSundae].descricao
+    }
+
+    let pedidos = [];
+
+    pedidos = localStorage.getItem("pedidos") == null ? [] : JSON.parse(localStorage.getItem("pedidos"));
+    pedidos.push(pedido);
+    console.log(pedidos);
+    localStorage.setItem("pedidos", JSON.stringify(pedidos));
+    publicarHtml();
+}
+
+
+function formatNumberMonetario(valorFinal) {
+    return valorFinal.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
+}
+
+//pedido cupuaçu
+function pedidoCupuacu() {
+
+    let tipoCupuacu = $('input[name=tipoCupuacu]:checked').val();
+    let complCupuacu = returnComplementValues('.input_value_cupuacu');
+    const Cupuacu = [];
+    Cupuacu["300ml"] = { 'valor': 12, 'descricao': 'Cupuaçu 300ml' };
+    Cupuacu["400ml"] = { 'valor': 15, 'descricao': 'Cupuaçu 400ml' };
+    Cupuacu["500ml"] = { 'valor': 18, 'descricao': 'Cupuaçu 500ml' };
+    Cupuacu["700ml"] = { 'valor': 25, 'descricao': 'Cupuaçu 700ml' };
+    //Cupuacu["leiteNinho"] = { 'valor': 3, 'descricao': 'leiteNinho' };
+
+    let pedido = {
+        "tipoPedido": tipoCupuacu,
+        "sabor": JSON.stringify(complCupuacu),
+        "valor": Cupuacu[tipoCupuacu].valor,
+        "descricao": Cupuacu[tipoCupuacu].descricao
+    }
+
+    let pedidos = [];
+
+    pedidos = localStorage.getItem("pedidos") == null ? [] : JSON.parse(localStorage.getItem("pedidos"));
+    pedidos.push(pedido);
+    localStorage.setItem("pedidos", JSON.stringify(pedidos));
+    publicarHtml();
+}
+
+function returnComplementValues(classCssComplemento) {
+    let complementos = [];
+    let values = document.querySelectorAll(classCssComplemento);
+
+    for (var i = 0; i < values.length; i++) {
+        if (values[i].checked) {
+            let object = {
+                'complement': values[i].value,
+                'price': $(values[i]).data("price")
+            }
+            complementos.push(object);
+        }
+    }
+
+    return complementos;
+}
+
+function formatNumberMonetario(valorFinal) {
+    return valorFinal.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
+}
+
+//pedido milkshake
+function pedidoMilkShake() {
+
+    let tipoMilshake = $('input[name=tipoMilkshake]:checked').val();
+    let saborMilkShake = $('input[name=saborMilkshake]:checked').val();
+
+    const milkShakes = [];
+    milkShakes["300ml"] = { 'valor': 8, 'descricao': 'Milk Shake 300ml' };
+    milkShakes["400ml"] = { 'valor': 10, 'descricao': 'Milk Shake 400ml' };
+    milkShakes["500ml"] = { 'valor': 12, 'descricao': 'Milk Shake 500ml' };
+
+    let pedido = {
+        "tipoPedido": tipoMilshake,
+        "sabor": saborMilkShake,
+        "valor": milkShakes[tipoMilshake].valor,
+        "descricao": milkShakes[tipoMilshake].descricao
+    }
+
+    let pedidos = [];
+
+    pedidos = localStorage.getItem("pedidos") == null ? [] : JSON.parse(localStorage.getItem("pedidos"));
+    pedidos.push(pedido);
+    localStorage.setItem("pedidos", JSON.stringify(pedidos));
+    publicarHtml();
+}
+
+
+function formatNumberMonetario(valorFinal) {
+    return valorFinal.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
+}
+
+//pedidos açai
+function pedidoAcai() {
+
+    let tipoAcai = $('input[name=tipoAcai]:checked').val();
+    let complAcai = $('input[name=complAcai]:checked').val();
+
+    const acais = [];
+    acais["300ml"] = { 'valor': 12, 'descricao': 'Açaí 300ml' };
+    acais["400ml"] = { 'valor': 15, 'descricao': 'Açaí 400ml' };
+    acais["500ml"] = { 'valor': 18, 'descricao': 'Açaí 500ml' };
+    acais["700ml"] = { 'valor': 25, 'descricao': 'Açaí 700ml' };
+
+    let pedido = {
+        "tipoPedido": tipoAcai,
+        "sabor": complAcai,
+        "valor": acais[tipoAcai].valor,
+        "descricao": acais[tipoAcai].descricao
+    }
+
+    let pedidos = [];
+
+    pedidos = localStorage.getItem("pedidos") == null ? [] : JSON.parse(localStorage.getItem("pedidos"));
+    pedidos.push(pedido);
+    localStorage.setItem("pedidos", JSON.stringify(pedidos));
+    publicarHtml();
 }
 
 function formatNumberMonetario(valorFinal) {
